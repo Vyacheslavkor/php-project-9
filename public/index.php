267b3bf20d0cd5a8784291b53f6bd4c3b1486768
Vyacheslav Korolev <vkorolev@cs-cart.com>
2023-01-15 14:57:53 +0400

    Template added.

--- public/index.php
+++ public/index.php
@@ -2,14 +2,22 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use DI\Container;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+
+$container = new Container();
+AppFactory::setContainer($container);
+
+$container->set('view', fn() => Twig::create(__DIR__ . '/../templates/', ['cache' => __DIR__ . '/../var/cache']));
 
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
+$app->add(TwigMiddleware::createFromContainer($app));
 
 $app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!');
-    return $response;
+    return $this->get('view')->render($response, 'index.twig');
 });
 
 $app->run();
